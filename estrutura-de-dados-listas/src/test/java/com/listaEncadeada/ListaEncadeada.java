bf2c5ec93bfc4829adead26bc9a1b80b24bbb6f5
Pedro Vaz <pedrovazs@outlook.com>
2022-01-10 15:59:10 -0300

    "Cria método add e getNo"

--- estrutura-de-dados-listas/src/test/java/com/listaEncadeada/ListaEncadeada.java
+++ estrutura-de-dados-listas/src/test/java/com/listaEncadeada/ListaEncadeada.java
@@ -7,6 +7,49 @@ public class ListaEncadeada<T> {
         this.refEntrada = null;
     }
 
+    // Adiciona um novo elemento a lista
+    public void add(T conteudo) {
+        // Cria o objeto no com o conteudo dentro
+        No<T> novoConteudo = new No<>(conteudo);
+        // Testa se a lista está vazia
+        if (this.isEmpty()) {
+            // Se for true a referência de entrada se torna o conteúdo passado
+            this.refEntrada = novoConteudo;
+            return;
+        }
+        // Se a lista não estiver vazia então a referência de entrada e colocada dentro da
+        // variável auxiliar
+        No<T> noAuxiliar = refEntrada;
+        // A estrutura 'for' percorre toda a lista até chegar no último elemento
+        for (int i = 0; i < this.size() - 1; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        // Quando chega ao fim usamos o método setProximoNo para colocarmos o elemento no fim da lista.
+        noAuxiliar.setProximoNo(novoConteudo);
+    }
+
+    // Pego um nó de acordo com o índice passado
+    private No<T> getNo(int index) {
+        // Testa se o índice passado é maior que o tamanho da lista
+        if (index >= this.size()) {
+            // Se for, lança um erro
+            throw new IndexOutOfBoundsException("Índice Inexistente!");
+        }
+        // Se não continua...
+        // Cria uma variável auxiliar
+        No<T> noAuxiliar = refEntrada;
+        // Cria uma variável para o retorno
+        No<T> noRetorno = null;
+        // Percorre a lista até o índice passado
+        for (int i = 0; i <= index; i++) {
+            // Guarda o valor da variável auxiliar na variável de retorno
+            noRetorno = noAuxiliar;
+            // Depois pega o próximo da lista
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
     // Mostra o tamanho da lista
     public int size() {
         int total = 0;
@@ -31,5 +74,12 @@ public class ListaEncadeada<T> {
     // Mostra se a lista está vazia ou não
     public boolean isEmpty() {
         return refEntrada == null;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada{" +
+                "refEntrada=" + refEntrada +
+                '}';
     }
 }
---
+++ estrutura-de-dados-listas/src/test/java/com/listaEncadeada/Main.java
@@ -0,0 +1,11 @@
+package com.listaEncadeada;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaEncadeada<String> lista = new ListaEncadeada<>();
+
+        lista.add("Primeiro");
+        lista.add("Segundo");
+        lista.add("Terceiro");
+    }
+}
